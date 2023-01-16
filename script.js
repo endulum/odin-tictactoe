7a6d7b0cd7ab9a5ca0f4ea08d98da56d73d1dd26
endulum <apheum@zoho.com>
2023-01-16 12:40:48 -0800

    divided game into display moduleand game module

--- script.js
+++ script.js
@@ -1,30 +1,72 @@
-function Gameboard() {
-    let board = document.getElementById('board');
+function Gameplay() {
     let tiles = [];
-    
-    this.clearBoard =()=> {
-        let child = board.lastElementChild;
-        while (child) {
-            board.removeChild();
-            child = board.lastElementChild;
-        };
+    let gameboard = new Gameboard();
+
+    this.wipeGame =()=> {
         tiles = [];
-    };
-    
-    this.populateBoard =()=> {
-        for (let i = 0; i < 9; i++) {
-            let button = document.createElement('button');
-            tiles[i] = '';
-            button.addEventListener('click', function() {
-                button.textContent = 'X';
-                button.disabled = 'true';
-                tiles[i] = 'X';
-                console.log(tiles);
-            });
-            board.appendChild(button);
-        };
-    };
+        gameboard.clearBoard();
+        console.log('Game is wiped.');
+    }
+
+    this.newGame =()=> {
+        tiles = ['', '', '', '', '', '', '', '', ''];
+        gameboard.populateGameWith(tiles);
+        console.log('Game is created.');
+    }
 }
 
-const game = new Gameboard();
-game.populateBoard();
+function Gameboard() {
+    let board = document.getElementById('board');
+
+    this.clearBoard =()=> {
+        let tile = board.lastElementChild;
+        while (tile) {
+            board.removeChild();
+            tile = board.lastElementChild;
+        }
+        console.log('Board is wiped.');
+    }
+
+    this.populateGameWith =(tiles)=> {
+        for (let tile in tiles) {
+            let button = document.createElement('button');
+            board.appendChild(button);
+        }
+    }
+}
+
+const game = new Gameplay();
+game.newGame();
+
+// game.wipeGame();
+
+// function Gameboard() {
+//     let board = document.getElementById('board');
+//     let tiles = [];
+    
+//     this.clearBoard =()=> {
+//         let child = board.lastElementChild;
+//         while (child) {
+//             board.removeChild();
+//             child = board.lastElementChild;
+//         };
+//         tiles = [];
+//     };
+    
+//     this.populateBoard =()=> {
+//         for (let i = 0; i < 9; i++) {
+//             let button = document.createElement('button');
+//             tiles[i] = '';
+//             button.addEventListener('click', function() {
+//                 button.textContent = 'X';
+//                 button.disabled = 'true';
+//                 tiles[i] = 'X';
+//                 console.log(tiles);
+//             });
+//             board.appendChild(button);
+//         };
+//     };
+// }
+
+// const game = new Gameboard();
+// game.populateBoard();
