725a616d2d3988ab1a0a5d769773de3efa1fec5a
endulum <apheum@zoho.com>
2023-01-22 19:32:50 -0800

    remade display control logic

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 NodeList.prototype.indexOf = Array.prototype.indexOf;
-// shoutout to stackoverflow
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+} // shoutout to stackoverflow
 
 const Player =(name, marker, isAI)=> {
     return {name, marker, isAI};
@@ -7,51 +9,99 @@ const Player =(name, marker, isAI)=> {
 
 const Game =(()=> {
     let tiles = [];
-    let flagReset = false;
+    let needsReset = false;
 
     const p1 = Player('Bob', 'O', false); // <img src="/resources/o.svg">
-    const p2 = Player('Eve', 'X', false); // <img src="/resources/x.svg">
+    const p2 = Player('Eve', 'X', true); // <img src="/resources/x.svg">
 
     const setP1Mode = function(mode) {
         p1.isAI = mode;
     }; const setP2Mode = function(mode) {
         p2.isAI = mode;
-    } // more elegant way to write this?
+    }
 
     let whoseTurn = p1;
 
-    const combos = [[0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical combos
-                    [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal combos
-                    [0, 4, 8], [2, 4, 6]]; // diagonal combos
+    const combos = [[0, 3, 6], [1, 4, 7], [2, 5, 8],
+                    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+                    [0, 4, 8], [2, 4, 6]];
+
+    const Board =(()=> {
+        const board = document.getElementById('board');
+        let buttons;
+
+        const clear = function() {
+            console.warn('Emptying board.');
+            let button = board.lastElementChild;
+            while (button) {
+                board.removeChild(button);
+                button = board.lastElementChild;
+            }
+        }
+
+        const populate = function(tiles) {
+            console.warn('Creating new board.');
+            for (let tile in tiles) {
+                let button = document.createElement('button');
+                button.addEventListener('click', select.bind(Board, parseInt(tile)));
+                button.classList.add('unclicked');
+                board.appendChild(button);
+            }
+        }
+
+        const select = function(index) {
+            Board.buttons[index].innerHTML = whoseTurn.marker;
+            Board.buttons[index].disabled = true;
+            Board.buttons[index].classList.remove('unclicked');
+            Board.buttons[index].classList.add('clicked');
+            // 'buttons' does not work but 'Board.buttons' does. why?
+            makeChoice(index);
+        }
+
+        const highlight = function(combo) {
+            for (let n of combo) {
+                Board.buttons[n].classList.add('winner');
+            }
+            togglePlayable();
+        }
+
+        const togglePlayable = function() {
+            document.querySelectorAll('.unclicked').forEach((b) => {
+                b.disabled = (b.disabled == true) ? false : true;
+            });
+        }
+
+        return {clear, populate, buttons, highlight, togglePlayable};
+    })();
 
     const setGame = function() {
+        if (needsReset) Board.clear();
         tiles = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+        Board.populate(tiles);
         whoseTurn = p1;
-        console.warn('New game created.');
-        console.log(tiles);
+        needsReset = true;
+        Board.buttons = document.querySelectorAll('#board > button');
     }
-
-    setGame(); // set the game once when page loads
 
     const makeChoice = function(choice) {
         if (tiles[choice] === ' ') {
             tiles[choice] = whoseTurn.marker;
-            console.warn(whoseTurn.name + " placed " + whoseTurn.marker + " on index " + choice);
-            console.log(tiles);
-            if (!tiles.includes(' ')) {
+            console.log(whoseTurn.name + " placed " + whoseTurn.marker + " on index " + choice);
+            if (checkForCombo()) {
+                console.warn(whoseTurn.name + ' got 3 in a row!');
+            } else if (!tiles.includes(' ')) {
                 console.warn('There are no free spaces. It\'s a draw.');
-                setGame();
-            } else if (checkForCombo()) {
-                console.warn(whoseTurn.name + " got 3 in a row!");
-                setGame();
             } else {
                 whoseTurn = (whoseTurn == p1) ? p2 : p1 ;
-                if (whoseTurn.isAI) {
-                    let freeSpace = Math.floor(Math.random()*9);
-                    while (tiles[freeSpace] !== ' ') {
-                        freeSpace = Math.floor(Math.random()*9);
-                    }
-                    makeChoice(freeSpace);
+                if (whoseTurn.isAI) { // give time to let AI "think"
+                    Board.togglePlayable();
+                    let freeSpace;
+                    do {freeSpace = Math.floor(Math.random()*9);}
+                    while (tiles[freeSpace] !== ' ');
+                    sleep(500).then(() => {
+                        Board.togglePlayable();
+                        Board.buttons[freeSpace].click();
+                    });
                 }
             }
         } else {
@@ -64,24 +114,27 @@ const Game =(()=> {
         for (let tile in tiles) {
             if (tiles[tile] === whoseTurn.marker) tileset.push(parseInt(tile));
         }
-
-        console.log(whoseTurn.name + ' currently has tiles in indices ' + tileset);
-
         for (let combo of combos) {
             let counter = 0;
             for (let n of combo) {
                 if (tileset.includes(n)) counter++;
-                if (counter === 3) return true;
+                if (counter === 3) {
+                    Board.highlight(combo);
+                    return true;
+                }
             }
         }
     }
 
+    setGame();
+
     return {makeChoice, setGame, setP1Mode, setP2Mode};
 })();
 
-// secret function to allow both AIs to fight to the death. this is the most important part of the project.
 const fight = function() {
     Game.setP1Mode(true);
     Game.setP2Mode(true);
-    Game.makeChoice(Math.floor(Math.random()*9));
+
+    let choices = document.querySelectorAll('.unclicked');
+    choices[Math.floor(Math.random()*9)].click();
 }
